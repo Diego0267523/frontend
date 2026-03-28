5ac7422c30761d6210eac8118e751ed54f2e7ac8
Diego <diegocamacho0074@gmail.com>
2026-03-28 05:49:20 -0500

    socket IA 3 login

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://gym-app-gytx.onrender.com";
-// const API_URL = "http://localhost:10000";  // Use only for local development
+// const API_URL = "https://gym-app-gytx.onrender.com";
+const API_URL = "http://localhost:9090";  // Use only for local development
 export default API_URL;
